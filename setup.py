93356d492f4ccf181d43dff91822d57840c19039
Kilian <kilian.lieret@posteo.de>
2020-03-09 18:46:13 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+# std
+from distutils.core import setup
+
+# noinspection PyUnresolvedReferences
+import setuptools  # see below (1)
+from pathlib import Path
+
+# (1) see https://stackoverflow.com/questions/8295644/
+# Without this import, install_requires won't work.
+
+
+keywords = [
+    "zettelkasten"
+]
+
+description = (
+    "Helper scripts for markdown based Zettelkasten."
+)
+
+this_dir = Path(__file__).resolve().parent
+
+packages = setuptools.find_packages()
+
+with (this_dir / "README.rst").open() as fh:
+    long_description = fh.read()
+
+with (this_dir / "verzettler" / "version.txt").open() as vf:
+    version = vf.read()
+
+with (this_dir / "requirements.txt").open() as rf:
+    requirements = [
+        req.strip()
+        for req in rf.readlines()
+        if req.strip() and not req.startswith("#")
+    ]
+
+
+setup(
+    name="verzettler",
+    version=version,
+    packages=packages,
+    install_requires=requirements,
+    url="https://github.com/klieret/verzettler",
+    project_urls={
+        "Bug Tracker": "https://github.com/klieret/verzettler/issues",
+        "Source Code": "https://github.com/klieret/verzettler/",
+    },
+    package_data={"verzettler": ["version.txt"]},
+    keywords=keywords,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+)
--- verzettler.py
+++
@@ -1,1 +0,0 @@
-# hih world
