0bca8318ea2fc2258b8d1dd29f29a1146a7816b7
Kilian <kilian.lieret@posteo.de>
2020-03-14 15:33:21 +0100

    Implement tag transform

--- verzettler/zettel.py
+++ verzettler/zettel.py
@@ -2,9 +2,13 @@
 
 # std
 import re
-from typing import List, Optional, Union, Set
+from typing import List, Optional, Union, Set, Iterable, Set
 from pathlib import Path, PurePath
 import os.path
+
+
+def identity(arg):
+    return arg
 
 
 class Zettel(object):
@@ -42,6 +46,18 @@ class Zettel(object):
         else:
             return path.name
 
+    # Parsing and formatting
+    # =========================================================================
+
+    @staticmethod
+    def _format_tags(tags: Iterable[str]) -> str:
+        return "Tags: " + " ".join("#"+tag for tag in tags)
+
+    def _read_tags(self, line: str) -> Set[str]:
+        return set(
+            t[1:] for t in set(self.tag_regex.findall(line))
+        )
+
     # Analyze file
     # =========================================================================
 
@@ -54,9 +70,7 @@ class Zettel(object):
                 if line.startswith("# "):
                     self.title = line.split("# ")[1].strip()
                 elif "tags: " in line.lower():
-                    self.tags = set(
-                        t[1:] for t in set(self.tag_regex.findall(line))
-                    )
+                    self.tags = self._read_tags(line)
                 elif line.startswith("##") and "backlinks" in line.lower():
                     backlinks_section = True
                 if backlinks_section:
@@ -64,10 +78,30 @@ class Zettel(object):
                 else:
                     self.links.extend(self.id_regex.findall(line))
 
-    def transform_file(self) -> None:
+    # Modify file
+    # =========================================================================
+
+    def transform_file(self, tag_transformer=identity) -> None:
         out_lines = []
         with self.path.open() as inf:
+            lastline = None
             for line in inf.readlines():
+
+                # Modifying tags if haven't been given before
+                if not self.tags:
+                    if lastline is not None and lastline.startswith("# "):
+                        out_lines.extend([
+                            "",
+                            self._format_tags(tag_transformer(set())),
+                            ""
+                        ])
+
+                # Modifying tags if already given
+                if "tags: " in line.lower():
+                    tags = tag_transformer(self._read_tags(line))
+                    line = self._format_tags(tags)
+
+                # Replacing/extending links
                 line = self.autogen_link_regex.sub("", line)
                 links = self.id_link_regex.findall(line)
                 for link in links:
@@ -82,7 +116,9 @@ class Zettel(object):
                     link_title = self.zettelkasten[zid].title
                     new_links = f"[[{zid}]] [{link_title}]({rel_path} \"autogen\")"
                     line = line.replace(link, new_links)
+
                 out_lines.append(line)
+                lastline = line
         with self.path.open("w") as outf:
             outf.writelines(out_lines)
 
