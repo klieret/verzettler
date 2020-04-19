5f7829066071c0d4858fc1e055f59a0b78c90e32
Kilian <kilian.lieret@posteo.de>
2020-04-19 10:46:27 +0200

    Simple addition of dot output

--- verzettler/bin/zk_web.py
+++ verzettler/bin/zk_web.py
@@ -3,9 +3,12 @@
 # std
 from pathlib import Path
 import argparse
+import sys
 
 # ours
 from verzettler.cli_util import init_zk_from_cli
+from verzettler.zettelkasten import Zettelkasten
+from verzettler.log import logger
 
 
 def format_dot_html(dot_str: str) -> str:
@@ -21,19 +24,40 @@ def format_dot_html(dot_str: str) -> str:
     return html
 
 
+def _output_html(zk: Zettelkasten, path: Path) -> None:
+    with path.open("w") as outf:
+        outf.write(format_dot_html(zk.dot_graph()))
+
+
+def _output_dot(zk: Zettelkasten, path: Path) -> None:
+    with path.open("w") as outf:
+        outf.write(zk.dot_graph())
+
+
+output_format_to_converter = {
+    ".html": _output_html,
+    ".dot": _output_dot
+}
+
+
 def cli():
 
     def add_additional_arguments(parser: argparse.ArgumentParser):
         parser.add_argument(
             "-o",
             "--output",
-            help="Output html file",
+            help=f"Output file. The following file formats are "
+                 f"supported: {', '.join(output_format_to_converter.keys())}.",
             default="out.html"
         )
 
     zk, args = init_zk_from_cli(additional_argparse_setup=add_additional_arguments)
-    with open(args.output, "w") as outf:
-        outf.write(format_dot_html(zk.dot_graph()))
+    args.output = Path(args.output)
+    try:
+        output_format_to_converter[args.output.suffix](zk, args.output)
+    except KeyError:
+        logger.critical(f"Unsupported suffix: {args.output.suffix}")
+        sys.exit(86)
 
 
 if __name__ == "__main__":
